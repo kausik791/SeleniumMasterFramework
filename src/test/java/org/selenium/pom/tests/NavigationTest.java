2552ee39ce08ee32c612d66389c7887285ddc8fa
kausik791 <kausik791@gmail.com>
2026-03-18 16:57:58 +0530

    changes in NavigationTest

--- src/test/java/org/selenium/pom/tests/NavigationTest.java
+++ src/test/java/org/selenium/pom/tests/NavigationTest.java
@@ -26,6 +26,6 @@ public class NavigationTest  extends BaseTest {
                 load().
                 getMyHeader().
                 navigateToStoreUsingMenu();
-        Assert.assertEquals(storePage.getTitle(), "Store25");
+        Assert.assertEquals(storePage.getTitle(), "Store35");
     }
 }
